176f5d062aacd66bea4424ade6c803a4be518372
Abdu4ik <107094143+Abdu4ik@users.noreply.github.com>
2022-08-20 04:44:26 +0500

    Update Divider.java

--- src/main/java/school/mjc/stage0/conditions/task3/Divider.java
+++ src/main/java/school/mjc/stage0/conditions/task3/Divider.java
@@ -2,5 +2,16 @@ package school.mjc.stage0.conditions.task3;
 
 public class Divider {
     public void isDividableBy5And11(int number) {
+
+        if (number%55==0){
+
+            System.out.println("Dividable");
+
+        }else{
+
+            System.out.println("Non-dividable");
+
+        }
+
     }
 }
